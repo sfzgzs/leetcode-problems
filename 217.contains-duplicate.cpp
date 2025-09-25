efee86c3e619c3515c4525512d450fa075958fe7
sfzgzs <ghasemi.fateme33@gmail.com>
2025-09-24 23:48:46 -0400

    Add 217-attemp2

--- 217.contains-duplicate.cpp
+++ 217.contains-duplicate.cpp
@@ -6,12 +6,27 @@ using namespace std;
 class Solution
 {
 public:
+    // 74 ms
+    // bool containsDuplicate(std::vector<int> &nums)
+    // {
+    //     std::unordered_set<int> myset(nums.begin(), nums.end());
+    //     if (nums.size() == myset.size())
+    //         return false;
+    //     else
+    //         return true;
+    // }
+
+    // 55 ms
     bool containsDuplicate(std::vector<int> &nums)
     {
-        std::unordered_set<int> myset(nums.begin(), nums.end());
-        if (nums.size() == myset.size())
-            return false;
-        else
-            return true;
+        unordered_set<int> mymap;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int size = mymap.size();
+            mymap.insert(nums[i]);
+            if (mymap.size() == size)
+                return true;
+        }
+        return false;
     }
 };
