ee080dc1d6d4088dba581cec9e5c6fd35ea0e884
sfzgzs <ghasemi.fateme33@gmail.com>
2025-10-30 11:27:46 -0400

    Improved Leetcode #206

--- 206.reverse-linked-list.cpp
+++ 206.reverse-linked-list.cpp
@@ -6,6 +6,15 @@
 
 using namespace std;
 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -19,14 +28,45 @@ using namespace std;
 class Solution
 {
 public:
+    // ListNode *reverseList(ListNode *head)
+    // {
+    //     if (head == nullptr || head->next == nullptr)
+    //         return head;
+
+    //     ListNode *newHead = reverseList(head->next);
+    //     head->next->next = head;
+    //     head->next = nullptr;
+    //     return newHead;
+    // }
+    ListNode *revHelp(ListNode *newone, ListNode *nextstep)
+    {
+        if (nextstep == nullptr)
+            return newone;
+
+        ListNode *next = nextstep->next;
+        nextstep->next = newone;
+        return revHelp(nextstep, next);
+    }
+
     ListNode *reverseList(ListNode *head)
     {
-        if (head == nullptr || head->next == nullptr)
-            return head;
-
-        ListNode *newHead = reverseList(head->next);
-        head->next->next = head;
-        head->next = nullptr;
-        return newHead;
+        return revHelp(nullptr, head);
     }
 };
+int main()
+{
+    Solution s;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+
+    ListNode *reversed = s.reverseList(head);
+
+    for (ListNode *p = reversed; p != nullptr; p = p->next)
+    {
+        cout << p->val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
