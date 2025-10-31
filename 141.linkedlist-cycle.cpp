cd960e47b05f2454ec020062b6ad6573d7725d9f
sfzgzs <ghasemi.fateme33@gmail.com>
2025-10-31 15:44:17 -0400

    Update leetcode #141

--- 141.linkedlist-cycle.cpp
+++ 141.linkedlist-cycle.cpp
@@ -6,22 +6,47 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <unordered_set>
 
-class Solution {
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
 public:
-    bool hasCycle(ListNode *head) {
-        if (!head || !head->next) return false; // empty or single node
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while (fast && fast->next) {
-            slow = slow->next;          // move one step
-            fast = fast->next->next;    // move two steps
-            
-            if (slow == fast) return true; // cycle detected
+    // Space O(1)
+    // bool hasCycle(ListNode *head) {
+    //     if (!head || !head->next) return false; // empty or single node
+
+    //     ListNode* slow = head;
+    //     ListNode* fast = head;
+
+    //     while (fast && fast->next) {
+    //         slow = slow->next;          // move one step
+    //         fast = fast->next->next;    // move two steps
+
+    //         if (slow == fast) return true; // cycle detected
+    //     }
+
+    //     return false; // no cycle
+    // }
+
+    // Space O(n)
+    bool hasCycle(ListNode *head)
+    {
+        unordered_set<ListNode *> prev;
+        ListNode *next = head;
+        while (next != nullptr)
+        {
+            if (prev.find(next) != prev.end())
+                return true;
+            prev.insert(next);
+            next = next->next;
         }
-        
-        return false; // no cycle
+        return false;
     }
 };
