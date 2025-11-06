a0d02c592e76883d733233080c47622e6512ca68
sfzgzs <ghasemi.fateme33@gmail.com>
2025-11-06 13:12:03 -0500

    Add leetcode #739

---
+++ 739.daily-tempratures.cpp
@@ -0,0 +1,53 @@
+#include <stack>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void printstack(stack<int> st)
+    {
+        stack<int> tempStack = st;
+
+        while (!tempStack.empty())
+        {
+            cout << tempStack.top() << " ";
+            tempStack.pop();
+        }
+        cout << endl;
+    }
+
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        stack<pair<int, int>> stc;
+        vector<int> res(temperatures.size(), 0);
+
+        for (int tIndex = 0; tIndex < temperatures.size(); tIndex++)
+        {
+            while (!stc.empty() && temperatures[tIndex] > stc.top().first)
+            {
+                pair<int, int> topElem = stc.top();
+                stc.pop();
+                res[topElem.second] = tIndex - topElem.second;
+            }
+            stc.push(pair<int, int>(temperatures[tIndex], tIndex));
+        }
+
+        return res;
+    }
+};
+void printvector(vector<int> st)
+{
+    for (int i = 0; i < st.size(); i++)
+        cout << st[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    Solution s;
+    vector<int> all = {73, 74, 75, 71, 69, 72, 76, 73};
+    printvector(s.dailyTemperatures(all));
+}
