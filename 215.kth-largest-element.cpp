afd7e6f27f662167d374449ff9793f0b9b43eee2
sfzgzs <ghasemi.fateme33@gmail.com>
2025-11-10 15:18:20 -0500

    Modified leetcode #215 to also work with Neetcode

--- 215.kth-largest-element.cpp
+++ 215.kth-largest-element.cpp
@@ -6,15 +6,35 @@ using namespace std;
 class Solution
 {
 public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
     int findKthLargest(vector<int> &nums, int k)
     {
-        priority_queue<int, vector<int>, greater<int>> pq;
+        this->k = k;
         for (int n : nums)
         {
             pq.push(n);
             if ((int)pq.size() > k)
                 pq.pop();
         }
+        return pq.top();
+    }
+    // Added the two functions below to adhrer to Neetcode "Kth Largest Element in a Stream" problem specs
+    void KthLargest(int k, vector<int> &nums)
+    {
+        this->k = k;
+        for (int n : nums)
+        {
+            pq.push(n);
+            if ((int)pq.size() > k)
+                pq.pop();
+        }
+    }
+    int add(int val)
+    {
+        pq.push(val);
+        if (pq.size() > k)
+            pq.pop();
         return pq.top();
     }
 };
