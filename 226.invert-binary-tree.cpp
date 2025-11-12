82e50197d9aa0e523d1ffee4495a800216501bc3
sfzgzs <ghasemi.fateme33@gmail.com>
2025-11-11 22:07:45 -0500

    Add leetcode #226

---
+++ 226.invert-binary-tree.cpp
@@ -0,0 +1,28 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == nullptr || (root->left == nullptr && root->right == nullptr))
+            return root;
+        TreeNode *tmp = invertTree(root->left);
+        root->left = invertTree(root->right);
+        root->right = tmp;
+        return root;
+    }
+};
+
+int main()
+{
+    return 0;
+}
