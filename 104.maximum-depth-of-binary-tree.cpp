9e2f04ab0455fb09da3c3bf83cd295a774b24db8
sfzgzs <ghasemi.fateme33@gmail.com>
2025-11-11 22:08:31 -0500

    Add leetcode#104

---
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    class Solution
+    {
+    public:
+        int maxDepth(TreeNode *root)
+        {
+            if (root == nullptr)
+                return 0;
+            return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        }
+    };
+};
+
+int main()
+{
+    return 0;
+}
