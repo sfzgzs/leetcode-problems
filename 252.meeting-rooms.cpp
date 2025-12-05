ccdd6c293ab1b584c1f64feab288b8058ede4c99
sfzgzs <ghasemi.fateme33@gmail.com>
2025-12-05 16:07:24 -0500

    Add Leetcode #252

--- 252.meeting-rooms.cpp
+++ 252.meeting-rooms.cpp
@@ -15,43 +15,62 @@ public:
         end = f;
     }
 };
-class Solution
-{
-public:
-    bool canAttendMeetings(vector<Interval> &intervals)
-    {
-        if (intervals.size() <= 1)
-            return true;
-
-        sort(intervals.begin(), intervals.end(),
-             [](const Interval &a, const Interval &b)
-             {
-                 return a.start < b.start;
-             });
-
-        for (int i = 1; i < intervals.size(); i++)
-        {
-            // if current start < previous end → overlap
-            if (intervals[i].start < intervals[i - 1].end)
-                return false;
-        }
-
-        return true;
-    }
-};
-
-void printvector(vector<int> st)
+void printvector(const vector<int> &st)
 {
     for (int i = 0; i < st.size(); i++)
         cout << st[i] << " ";
     cout << endl;
 }
+void printwhatsleftof(vector<int> &st, int index)
+{
+    for (int i = index; i < st.size(); i++)
+        cout << st[i] << " ";
+    cout << endl;
+}
+class Solution
+{
+public:
+    int minMeetingRooms(vector<Interval> &intervals)
+    {
+        vector<int> starts(intervals.size());
+        vector<int> ends(intervals.size());
+
+        transform(intervals.begin(), intervals.end(), starts.begin(), [](const Interval &intrv)
+                  { return intrv.start; });
+        transform(intervals.begin(), intervals.end(), ends.begin(), [](const Interval &intrv)
+                  { return intrv.end; });
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+        // printvector(starts);
+        // printvector(ends);
+        int startsptr = 0;
+        int endptr = 0;
+        int count = 0;
+        int maxcount = 0;
+        for (int i = 0; i < intervals.size(); i++)
+        {
+            cout << "start= " << starts[startsptr];
+            cout << ", end= " << ends[endptr] << endl;
+
+            if (starts[startsptr] < ends[endptr])
+            {
+                startsptr++;
+                count++;
+            }
+            else
+            {
+                endptr++;
+                count--;
+            }
+            maxcount = max(maxcount, count);
+        }
+        return maxcount;
+    }
+};
 
 int main()
 {
     Solution s;
-    vector<Interval> meetings = {Interval(8, 11),
-                                 Interval(10, 13),
-                                 Interval(20, 22)};
-    cout << s.canAttendMeetings(meetings);
+    vector<Interval> meetings = {{0, 10}, {10, 20}, {20, 30}, {30, 40}, {40, 50}, {50, 60}, {60, 70}, {70, 80}, {80, 90}, {90, 100}, {0, 100}, {10, 90}, {20, 80}, {30, 70}, {40, 60}};
+    cout << s.minMeetingRooms(meetings);
 }
