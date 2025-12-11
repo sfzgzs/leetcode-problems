c3bb755afb41930758195cac7fbb624c131c3ff0
sfzgzs <ghasemi.fateme33@gmail.com>
2025-12-10 20:37:31 -0500

    Add leetcode #22

---
+++ 22.generate-parenthesis.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    int parnum = 0;
+    string ressofar = "";
+    vector<string> results;
+    void backtrack(int curropen, int currclose)
+    {
+        if (curropen == currclose && curropen == parnum)
+        {
+            results.push_back(ressofar);
+            return;
+        }
+        string tmp = ressofar;
+        if (curropen < parnum)
+        {
+            ressofar += "(";
+            backtrack(curropen + 1, currclose);
+        }
+        if (currclose < curropen)
+        {
+            ressofar = tmp;
+            ressofar += ")";
+            backtrack(curropen, currclose + 1);
+        }
+    }
+    vector<string> generateParenthesis(int n)
+    {
+        parnum = n;
+        backtrack(0, 0);
+        return results;
+    }
+};
+
+// void printvector(const vector<int> &st)
+// {
+//     for (int i = 0; i < st.size(); i++)
+//         cout << st[i] << " ";
+//     cout << endl;
+// }
+int main()
+{
+    Solution s;
+    auto res = s.generateParenthesis(3);
+    for (auto v : res)
+    {
+        cout << "V: " << v << endl;
+    }
+}
