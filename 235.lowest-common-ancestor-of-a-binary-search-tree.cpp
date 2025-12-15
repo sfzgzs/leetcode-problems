cc23e696ea45bc976ac2648052741f63b5b2bb91
sfzgzs <ghasemi.fateme33@gmail.com>
2025-12-15 11:14:13 -0500

    Add leetcode #235

---
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,26 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        TreeNode *current = root;
+        while (current != nullptr)
+        {
+            if (p->val < current->val && q->val < current->val)
+                current = current->left;
+            else if (p->val > current->val && q->val > current->val)
+                current = current->right;
+            else
+                return current;
+        }
+        return nullptr;
+    }
+};
