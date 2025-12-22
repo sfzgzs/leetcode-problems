64eee6ba1e0f61087e6302fbfb9c39465bb2b3f4
sfzgzs <ghasemi.fateme33@gmail.com>
2025-12-22 11:16:21 -0500

    Attenpt 2 leetcode#338

--- 338.counting-bits.cpp
+++ 338.counting-bits.cpp
@@ -12,4 +12,19 @@ public:
 
         return bits;
     }
+    // Second attempt
+    vector<int> countBits(int n)
+    {
+        int offset = 1;
+        vector<int> bits(n + 1, 0);
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (2 * offset == i)
+                offset *= 2;
+            bits[i] = 1 + bits[i - offset];
+        }
+
+        return bits;
+    }
 };
