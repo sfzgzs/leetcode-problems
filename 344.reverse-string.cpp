d0c0cdac8b896dfad0bab6663e599f93dc3fb51f
Fateme Ghasemi <ghasemi.fateme33@gmail.com>
2026-02-21 18:32:44 -0500

    Add leetcode #344 reverse string

---
+++ 344.reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0;
+        int r = (int)s.size() - 1;
+
+        while (l < r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+};
