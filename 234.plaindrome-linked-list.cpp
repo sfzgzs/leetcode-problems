3d9b987b310c4b7cbd972bca7e0d6491936f42ad
Fateme Ghasemi <ghasemi.fateme33@gmail.com>
2026-02-24 00:07:08 -0500

    add234.plaindrome-linked-list.cpp

---
+++ 234.plaindrome-linked-list.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* next = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        while (prev) {
+            if (head->val != prev->val) return false;
+            head = head->next;
+            prev = prev->next;
+        }
+
+        return true;
+    }
+};
