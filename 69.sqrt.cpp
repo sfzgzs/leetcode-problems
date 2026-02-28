c8d0da44c4318a8a32e80ffe1bccee183a6f86b0
Fateme Ghasemi <ghasemi.fateme33@gmail.com>
2026-02-27 23:15:48 -0500

    Add leetcode #69 sqrt

---
+++ 69.sqrt.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x <= 1)
+            return x;
+        
+        long long l = 1, r = x, ans = 0;
+        
+        while (l <= r) {
+            long long mid = l + (r - l) / 2;
+            
+            if (mid * mid <= x) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
