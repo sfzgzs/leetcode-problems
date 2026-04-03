74a84cdbd17eb94a71c55ffed8a211ef567f83d1
Fateme Ghasemi <ghasemi.fateme33@gmail.com>
2026-04-02 22:52:52 -0400

    Add leetcode# 283.move-zeroes.cpp

---
+++ 283.move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[pos] = nums[i];
+                pos = pos + 1;
+            }
+        }
+
+        for (int i = pos; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
