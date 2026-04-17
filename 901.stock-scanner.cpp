655a06861b905b515b4df6faa47eb92786c5e7ca
sfzgzs <ghasemi.fateme33@gmail.com>
2026-04-17 14:43:23 -0400

    Added Explanation to #901 stock scanner

--- 901.stock-scanner.cpp
+++ 901.stock-scanner.cpp
@@ -1,9 +1,33 @@
 #include <algorithm>
 #include <stack>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
+void printstack(stack<pair<int, int>> st)
+{
+    stack<pair<int, int>> tempStack;
+    stack<pair<int, int>> tempStack2;
+    while (!st.empty())
+    {
+        tempStack2.push(st.top());
+        st.pop();
+    }
+    while (!tempStack2.empty())
+    {
+        st.push(tempStack2.top());
+        tempStack.push(tempStack2.top());
+        tempStack2.pop();
+    }
+
+    while (!tempStack.empty())
+    {
+        cout << "(" << tempStack.top().first << ", " << tempStack.top().second << "), ";
+        tempStack.pop();
+    }
+    cout << endl;
+}
 class StockSpanner
 {
 public:
@@ -16,14 +40,30 @@ public:
     int next(int price)
     {
         int counter = 1;
-
-        while (!st.empty() && st.top().first <= price)
+        printstack(stocks);
+        while (!stocks.empty() && stocks.top().first <= price)
         {
-            counter += st.top().second;
-            st.pop();
+            cout << "popping " << stocks.top().first << ", " << stocks.top().second << endl;
+            counter += stocks.top().second;
+            stocks.pop();
         }
 
-        st.push({price, counter});
+        cout << "pushing " << price << ", " << counter << endl;
+
+        stocks.push({price, counter});
+        cout << "--------------------\n";
         return counter;
     }
 };
+int main()
+{
+    StockSpanner s;
+    s.next(100);
+    s.next(80);
+    s.next(60);
+    s.next(70);
+    s.next(60);
+    s.next(75);
+    s.next(85);
+    return 0;
+}
