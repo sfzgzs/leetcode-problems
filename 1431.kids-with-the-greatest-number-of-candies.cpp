eec7bce5c0f1ddc22ed9d107a80fd1b870dc23bd
sfzgzs <ghasemi.fateme33@gmail.com>
2026-04-16 22:49:25 -0400

    Simplified leetcode #1431

--- 1431.kids-with-the-greatest-number-of-candies.cpp
+++ 1431.kids-with-the-greatest-number-of-candies.cpp
@@ -10,19 +10,17 @@ class Solution
 public:
     vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
     {
-        vector<bool> res(candies.size(), false);
-        map<int, vector<int>> candies_kids; // should be sorted (increasing) by # of candies
-        for (int i = 0; i < candies.size(); i++)
-            candies_kids[candies[i]].push_back(i);
-        auto &lastElement = candies_kids.rbegin();
-        int maxCandy = lastElement.first() : for (auto &[candyCount, kidIndices] : candies_kids)
-        {
-            if (candyCount + extraCandies > maxCandy)
-                for (int kid : kidIndices)
-                {
-                    res[kid] = true;
-                }
-        }
+        int maxCandy = 0;
+
+        for (int c : candies)
+            maxCandy = max(maxCandy, c);
+
+        vector<bool> res;
+
+        for (int c : candies)
+            res.push_back(c + extraCandies >= maxCandy);
+
+        return res;
     }
 };
 int main()
