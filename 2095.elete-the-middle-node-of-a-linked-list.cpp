0a7e45e367b5cdabee30043acaf0c5dbd5ea2f6d
sfzgzs <ghasemi.fateme33@gmail.com>
2026-04-20 12:19:00 -0400

    Add 2095

---
+++ 2095.elete-the-middle-node-of-a-linked-list.cpp
@@ -0,0 +1,36 @@
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *deleteMiddle(ListNode *head)
+    {
+        if (!head || !head->next)
+            return nullptr;
+
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode *slow = dummy;
+        ListNode *fast = dummy;
+        ListNode *mid = head;
+
+        while (fast->next && fast->next->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return dummy->next;
+    }
+};
