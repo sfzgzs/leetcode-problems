15f80852e728188e6ebde100371e1bf6abbe8120
sfzgzs <ghasemi.fateme33@gmail.com>
2026-04-22 22:54:18 -0400

    Add leetcode 790

---
+++ 790.domino-and-tromino-tiling.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int numTilings(int n)
+    {
+        const int MOD = 1e9 + 7;
+
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        if (n == 3)
+            return 5;
+
+        long n1 = 1, n2 = 2, n3 = 5;
+
+        for (int i = 4; i <= n; i++)
+        {
+            long tmp = (2 * n3 + n1) % MOD;
+            n1 = n2;
+            n2 = n3;
+            n3 = tmp;
+        }
+
+        return n3;
+    }
+};
